66ed447cc2ce33c32638c3194405127ebeec0b69
Sandeep Shantharam <machbio@gmail.com>
2019-09-13 16:59:44 -0400

    testing out netifly cms 2

--- pelicanconf.py
+++ pelicanconf.py
@@ -82,6 +82,8 @@ CATEGORY_SAVE_AS = 'category/{slug}.html'
 # Tipue Search
 DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'authors', 'archives', 'search'))
 
+TEMPLATE_PAGES = {'admin/index.html': 'admin/index.html'}
+STATIC_PATHS = ['uploads', 'admin']
 
 SITEMAP = {
     'format': 'xml',
